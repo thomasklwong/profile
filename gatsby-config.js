5a0241819648bc1e5d5a43177525896f8a910951
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-04-02 21:17:34 +0800

    feat: tell gatsby-source-filesystem where markdown is
    * Also load gatsby-transformer-remark to transform markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+    },
+    `gatsby-plugin-netlify-cms`,
+  ],
 }
