6cc0063d424743b2b6baa7b0d1bf8e141ebfe6ed
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-04-03 23:25:26 +0800

    feat: add Author div

---
+++ src/components/Author/index.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { Link, StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image/withIEPolyfill"
+
+const query = graphql`
+  query AuthorQuery {
+    site {
+      siteMetadata {
+        author {
+          name
+          email
+          github
+          twitter
+          rss
+        }
+      }
+    }
+    file(relativePath: { eq: "Thomas-Wong.jpg" }) {
+      childImageSharp {
+        fixed(width: 75, height: 75) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
+
+const Author = ({ location: { pathname } = {} }) => {
+  const isRootPage = pathname === "/"
+  // eslint-disable-next-line  jsx-a11y/heading-has-content
+  const Heading = props => (isRootPage ? <h1 {...props} /> : <h2 {...props} />)
+
+  const render = ({
+    site: {
+      siteMetadata: {
+        author: { name, email, github, twitter, rss },
+      },
+    },
+    file: {
+      childImageSharp: { fixed },
+    },
+  }) => (
+    <div>
+      <Link to="/">
+        <Img
+          fixed={fixed}
+          alt={name}
+          objectFit="cover"
+          objectPosition="50% 50%"
+        />
+      </Link>
+      <Heading>
+        <Link to="/">{name}</Link>
+      </Heading>
+    </div>
+  )
+
+  return <StaticQuery query={query} render={render} />
+}
+
+export default Author
