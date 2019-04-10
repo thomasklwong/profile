bd02dab2409647d1ff42c830355c9d591943e981
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-04-10 22:31:34 +0800

    feat: use prettier to fix all supported format

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{js,jsx}': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.{md,html}': ['prettier --write', 'git add']
+  '*.{css,scss,html,json,md,yml}': ['prettier --write', 'git add']
 };
