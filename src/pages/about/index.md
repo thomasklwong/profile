6eaf738f1956561d6c5a4ca4f30c49e4e0353012
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-04-29 22:59:16 +0800

    feat: add about page and about-template

---
+++ src/pages/about/index.md
@@ -0,0 +1,7 @@
+---
+templateKey: 'about-page'
+path: /about
+title: About Me
+---
+
+I am a web engineer, have a good knowledge on front-end and back-end technologies and have a special focus on video streaming.
---
+++ src/templates/about-page.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes, { shape } from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
+
+export const query = graphql`
+  query AboutPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
+
+export const AboutPageTemplate = ({
+  title,
+  content,
+  contentComponent: PageContent = Content
+}) => (
+  <section>
+    <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+      {title}
+    </h2>
+    <PageContent className="content" content={content} />
+  </section>
+);
+
+AboutPageTemplate.propTypes = {
+  content: PropTypes.string,
+  contentComponent: PropTypes.element,
+  title: PropTypes.string.isRequired
+};
+
+const AboutPage = ({
+  data: {
+    markdownRemark: {
+      html,
+      frontmatter: { title }
+    }
+  }
+}) => (
+  <Layout>
+    <AboutPageTemplate
+      content={html}
+      contentComponent={HTMLContent}
+      title={title}
+    />
+  </Layout>
+);
+AboutPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      html: PropTypes.string,
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string.isRequired
+      })
+    })
+  })
+};
+
+export default AboutPage;
