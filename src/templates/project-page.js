9b4b63b7f0e66eb563a02d2dd75beef1fb9be0a6
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-05-03 23:12:44 +0800

    feat: add some structure to the project page

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import PropTypes, { shape } from 'prop-types';
+import Layout from '../components/Layout';
 
-const ProjectPage = () => <div />;
+export const query = graphql`
+  query ProjectPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        date
+        description
+      }
+    }
+  }
+`;
+
+const ProjectPage = ({
+  data: {
+    markdownRemark: {
+      frontmatter: { title, date, description }
+    }
+  }
+}) => (
+  <Layout>
+    <div>{title}</div>
+    <div>{date}</div>
+    <div>{description}</div>
+  </Layout>
+);
+
+ProjectPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired
+      })
+    })
+  })
+};
 
 export default ProjectPage;
