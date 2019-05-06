5e9451b08beeec026ddb5accf4c8dd798b765c21
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-05-06 21:14:42 +0800

    feat: partially reorg the page based on netlify cms example

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -1,43 +1,91 @@
 import React from 'react';
 import PropTypes, { shape } from 'prop-types';
+import { graphql, Link } from 'gatsby';
+import camelcase from 'camelcase';
+import decamelize from 'decamelize';
 import Layout from '../components/Layout';
+
+const kebabcase = str => decamelize(camelcase(str), '-');
 
 export const query = graphql`
   query ProjectPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
+      id
       html
       frontmatter {
         title
-        date
+        date(formatString: "dddd, D MMMM Y, h:mm:ss a")
         description
+        tags
       }
     }
   }
 `;
 
+export const ProjectPageTemplate = ({
+  content,
+  contentComponent,
+  date,
+  description,
+  tags,
+  title,
+  helmet
+}) => (
+  <article>
+    <helmet />
+    <h1>{title}</h1>
+    <p>{date}</p>
+    <p>{description}</p>
+    <contentComponent>{content}</contentComponent>
+    {tags?.length ? (
+      <section>
+        <h4>Tags</h4>
+        <ul>
+          {tags.map(kebabcase).map(tag => (
+            <li key={`tag-${tag}`}>
+              <Link to={`/tags/${tag}`}>{tag}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    ) : null}
+  </article>
+);
+
+ProjectPageTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func.isRequired,
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  helmet: PropTypes.node.isRequired,
+  tags: PropTypes.array,
+  title: PropTypes.string.isRequired
+};
+
 const ProjectPage = ({
   data: {
     markdownRemark: {
-      frontmatter: { title, date, description }
+      html,
+      frontmatter: { title, date, description, tags }
     }
   }
 }) => (
   <Layout>
-    <div>{title}</div>
-    <div>{date}</div>
-    <div>{description}</div>
+    <ProjectPageTemplate
+      content={html}
+      contentComponent={() => {}}
+      date={date}
+      description={description}
+      helmet={() => {}}
+      tags={tags}
+      title={title}
+    />
   </Layout>
 );
 
 ProjectPage.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired
-      })
-    })
+    markdownRemark: PropTypes.shape({})
   })
 };
 
