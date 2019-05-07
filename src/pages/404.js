7b1184193ef6ccf0c21803775a2cec48b70afdfa
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-05-07 21:50:11 +0800

    feat: simple 404

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const NotFoundPage = () => (
+  <Layout>
+    <article>
+      <h1>404</h1>
+      <p>What you are looking for does not exists.</p>
+    </article>
+  </Layout>
+);
+
+export default NotFoundPage;
