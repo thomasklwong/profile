681d7830f0258ad934dc6702a27445aa8adb736f
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-05-24 20:39:26 +0800

    feat: styled sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import tw from 'tailwind.macro';
+
 import Author from '../Author';
 import Nav from '../Nav';
 import ExternalLinks from '../ExternalLinks';
+
+const Aside = styled.aside`
+  ${tw`w-full md:w-2/5 lg:w-1/3 md:mr-6`};
+`;
 
 const Copyright = () => <small>{'© All rights reserved.'}</small>;
 
 const Sidebar = ({ location }) => {
   return (
-    <aside>
+    <Aside>
       <Author location={location} />
       <Nav />
       <ExternalLinks />
       <Copyright />
-    </aside>
+    </Aside>
   );
 };
 
