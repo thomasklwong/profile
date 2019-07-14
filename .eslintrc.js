bd99af5d7cc77246ad2a9c103fb893ee16bbe4f9
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-07-14 14:10:14 +0800

    chore: upgrade dep and directly copy eslint-config-react-app@4.0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
+// Directly copy and paste eslint-config-react-app here, with modification
+// that fit the project, as eslint-config-react-app@4.0.1 does not work with
+// ESLint 6 properly.
+const restrictedGlobals = require('confusing-browser-globals');
+
 module.exports = {
+  parser: 'babel-eslint',
+
   extends: [
     'eslint:recommended',
-    'react-app',
     'plugin:import/recommended',
     'plugin:import/react',
     'plugin:react/recommended',
@@ -11,7 +17,14 @@ module.exports = {
     'prettier/react'
   ],
 
-  plugins: ['react-hooks', 'emotion'],
+  plugins: ['import', 'jsx-a11y', 'react', 'react-hooks', 'emotion'],
+
+  env: {
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
 
   globals: {
     graphql: true,
@@ -34,62 +47,226 @@ module.exports = {
   },
 
   rules: {
-    'no-console': 0,
+    'array-callback-return': 'warn',
+    'default-case': ['warn', { commentPattern: '^no default$' }],
+    'dot-location': ['warn', 'property'],
+    eqeqeq: ['warn', 'smart'],
+    'new-parens': 'warn',
+    'no-array-constructor': 'warn',
+    'no-caller': 'warn',
+    'no-cond-assign': ['warn', 'except-parens'],
+    'no-const-assign': 'warn',
+    'no-control-regex': 'warn',
+    'no-delete-var': 'warn',
+    'no-dupe-args': 'warn',
+    'no-dupe-class-members': 'warn',
+    'no-dupe-keys': 'warn',
+    'no-duplicate-case': 'warn',
+    'no-empty-character-class': 'warn',
+    'no-empty-pattern': 'warn',
+    'no-eval': 'warn',
+    'no-ex-assign': 'warn',
+    'no-extend-native': 'warn',
+    'no-extra-bind': 'warn',
+    'no-extra-label': 'warn',
+    'no-fallthrough': 'warn',
+    'no-func-assign': 'warn',
+    'no-implied-eval': 'warn',
+    'no-invalid-regexp': 'warn',
+    'no-iterator': 'warn',
+    'no-label-var': 'warn',
+    'no-labels': ['warn', { allowLoop: true, allowSwitch: false }],
+    'no-lone-blocks': 'warn',
+    'no-loop-func': 'warn',
+    'no-mixed-operators': [
+      'warn',
+      {
+        groups: [
+          ['&', '|', '^', '~', '<<', '>>', '>>>'],
+          ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+          ['&&', '||'],
+          ['in', 'instanceof']
+        ],
+        allowSamePrecedence: false
+      }
+    ],
+    'no-multi-str': 'warn',
+    'no-native-reassign': 'warn',
+    'no-negated-in-lhs': 'warn',
+    'no-new-func': 'warn',
+    'no-new-object': 'warn',
+    'no-new-symbol': 'warn',
+    'no-new-wrappers': 'warn',
+    'no-obj-calls': 'warn',
+    'no-octal': 'warn',
+    'no-octal-escape': 'warn',
+    'no-redeclare': 'warn',
+    'no-regex-spaces': 'warn',
+    'no-restricted-syntax': ['warn', 'WithStatement'],
+    'no-script-url': 'warn',
+    'no-self-assign': 'warn',
+    'no-self-compare': 'warn',
+    'no-sequences': 'warn',
+    'no-shadow-restricted-names': 'warn',
+    'no-sparse-arrays': 'warn',
+    'no-template-curly-in-string': 'warn',
+    'no-this-before-super': 'warn',
+    'no-throw-literal': 'warn',
+    'no-undef': 'error',
+    'no-restricted-globals': ['error'].concat(restrictedGlobals),
+    'no-unexpected-multiline': 'warn',
+    'no-unreachable': 'warn',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true
+      }
+    ],
+    'no-unused-labels': 'warn',
+    'no-unused-vars': [
+      'warn',
+      {
+        args: 'none',
+        ignoreRestSiblings: true
+      }
+    ],
+    'no-use-before-define': [
+      'warn',
+      {
+        functions: false,
+        classes: false,
+        variables: false
+      }
+    ],
+    'no-useless-computed-key': 'warn',
+    'no-useless-concat': 'warn',
+    'no-useless-constructor': 'warn',
+    'no-useless-escape': 'warn',
+    'no-useless-rename': [
+      'warn',
+      {
+        ignoreDestructuring: false,
+        ignoreImport: false,
+        ignoreExport: false
+      }
+    ],
+    'no-with': 'warn',
+    'no-whitespace-before-property': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'require-yield': 'warn',
+    'rest-spread-spacing': ['warn', 'never'],
+    strict: ['warn', 'never'],
+    'unicode-bom': ['warn', 'never'],
+    'use-isnan': 'warn',
+    'valid-typeof': 'warn',
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'require',
+        property: 'ensure',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting'
+      },
+      {
+        object: 'System',
+        property: 'import',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting'
+      }
+    ],
+    'getter-return': 'warn',
 
-    'import/no-unresolved': 2,
-    'import/named': 2,
-    'import/default': 2,
-    'import/no-self-import': 2,
-    'import/no-cycle': 2,
-    'import/no-useless-path-segments': 2,
-    'import/export': 2,
-    'import/no-deprecated': 2,
-    'import/first': 2,
-    'import/no-duplicates': 2,
-    'import/extensions': 2,
-    'import/order': 2,
-    'import/newline-after-import': 2,
-    'import/no-unassigned-import': 2,
-    'import/no-amd': 2,
-    'import/no-webpack-loader-syntax': 2,
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'error',
+    'import/first': 'error',
+    'import/named': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-amd': 'error',
+    'import/no-cycle': 'error',
+    'import/no-deprecated': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-self-import': 'error',
+    'import/no-unassigned-import': 'error',
+    'import/no-unresolved': 'error',
+    'import/no-useless-path-segments': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/order': 'error',
 
-    'react/default-props-match-prop-types': 2,
-    'react/no-access-state-in-setstate': 2,
-    'react/no-array-index-key': 2,
-    'react/no-children-prop': 2,
-    'react/no-danger': 2,
-    'react/no-danger-with-children': 2,
-    'react/no-deprecated': 2,
-    'react/no-did-mount-set-state': 2,
-    'react/no-did-update-set-state': 2,
-    'react/no-direct-mutation-state': 2,
-    'react/no-find-dom-node': 2,
-    'react/no-is-mounted': 2,
-    'react/no-redundant-should-component-update': 2,
-    'react/no-render-return-value': 2,
-    'react/no-typos': 2,
-    'react/no-string-refs': 2,
-    'react/no-unescaped-entities': 2,
-    'react/no-unknown-property': 2,
-    'react/no-unsafe': 2,
-    'react/no-unused-prop-types': 2,
-    'react/no-unused-state': 2,
-    'react/no-will-update-set-state': 2,
-    'react/prefer-stateless-function': [2, { ignorePureComponents: true }],
-    'react/react-in-jsx-scope': 2,
-    'react/self-closing-comp': 2,
-    'react/sort-comp': 2,
-    'react/sort-prop-types': 2,
-    'react/style-prop-object': 2,
-    'react/void-dom-elements-no-children': 2,
-    'react/jsx-key': 2,
-    'react/jsx-pascal-case': 2,
-    'react/jsx-sort-default-props': 2,
-    'react/jsx-sort-props': 2,
+    'react/default-props-match-prop-types': 'error',
+    'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+    'react/jsx-key': 'error',
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-sort-default-props': 'error',
+    'react/jsx-sort-props': 'error',
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/no-access-state-in-setstate': 'error',
+    'react/no-array-index-key': 'error',
+    'react/no-children-prop': 'error',
+    'react/no-danger': 'error',
+    'react/no-danger-with-children': 'error',
+    'react/no-deprecated': 'error',
+    'react/no-did-mount-set-state': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-find-dom-node': 'error',
+    'react/no-is-mounted': 'error',
+    'react/no-redundant-should-component-update': 'error',
+    'react/no-render-return-value': 'error',
+    'react/no-typos': 'error',
+    'react/no-string-refs': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-unknown-property': 'error',
+    'react/no-unsafe': 'error',
+    'react/no-unused-prop-types': 'error',
+    'react/no-unused-state': 'error',
+    'react/no-will-update-set-state': 'error',
+    'react/prefer-stateless-function': [
+      'error',
+      { ignorePureComponents: true }
+    ],
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
+    'react/self-closing-comp': 'error',
+    'react/sort-comp': 'error',
+    'react/sort-prop-types': 'error',
+    'react/style-prop-object': 'error',
+    'react/void-dom-elements-no-children': 'error',
 
-    'react-hooks/rules-of-hooks': 2,
-    'react-hooks/exhaustive-deps': 2,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
 
-    'emotion/jsx-import': 2
+    'jsx-a11y/accessible-emoji': 'warn',
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-has-content': 'warn',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['noHref', 'invalidHref']
+      }
+    ],
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/no-access-key': 'warn',
+    'jsx-a11y/no-distracting-elements': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/scope': 'warn',
+
+    'emotion/jsx-import': 'error'
   }
 };
