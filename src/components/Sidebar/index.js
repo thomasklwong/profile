3b6312faeca150d0040f4d66225c1a623ff3275b
Thomas Wong <thomasklwong@users.noreply.github.com>
2019-08-11 23:38:20 +0800

    feat: add vertical hairline

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import tw from 'tailwind.macro';
 
 import Author from '../Author';
 import Nav from '../Nav';
 import ExternalLinks from '../ExternalLinks';
 
-const Aside = tw.aside`w-full p-5 md:w-1/5 md:mr-6`;
+const Aside = styled.aside`
+  ${tw`w-full p-5 md:w-1/5 md:mr-6`}
+
+  ::after {
+    content: '';
+    position: relative;
+    display: block;
+    left: calc(100% + 1.125rem);
+    top: calc(-100% + 32rem - 1.25rem);
+    width: 1px;
+    height: 32rem;
+    background: linear-gradient(to bottom, white 0, #e6e6e6 48%, white 100%);
+  }
+`;
+
 const StyledSmall = tw.small`block mt-10 text-gray-500`;
 const Copyright = () => <StyledSmall>{'© All rights reserved.'}</StyledSmall>;
 
